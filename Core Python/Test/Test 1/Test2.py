b88f2ea4cc3c124bbac0e966619d5c5b76298154
Siddesh Kharbude <130760615+siddesh1013@users.noreply.github.com>
2026-08-31 16:10:46 +0530

    Delete Core Python/Test/Test 1 directory

--- Core Python/Test/Test 1/Test2.py
+++
@@ -1,9 +0,0 @@
-### Write a program to calculate simple interest based on Principal, Rate and Time (SI = P*R*T/100)
-
-Principal_amount = int(input('Enter Principal Amount:'))
-Time_Years = int(input('Enter Time in Years:'))
-Rate_of_Interest = int(input('Enter Rate of Interest:'))
-
-Simple_Interest = (Principal_amount * Time_Years * Rate_of_Interest) / 100
-
-print(f'Simple Interest: {Simple_Interest}')
